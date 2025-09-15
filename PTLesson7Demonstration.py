97a8674edff10da06fe135534cf844b15cec29ed
Renne Castro <rcastro2@users.noreply.github.com>
2025-09-15 04:01:50 -0400

    adds Lesson 7 Demonstration

---
+++ PTLesson7Demonstration.py
@@ -0,0 +1,13 @@
+import turtle
+
+bob = turtle.Turtle()
+
+angle = 120
+distance = 100
+
+bob.forward(distance)
+bob.left(angle)
+bob.forward(distance)
+bob.left(angle)
+bob.forward(distance)
+bob.left(angle)
