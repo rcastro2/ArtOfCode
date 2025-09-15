1ef75905cdc7dc5a5693bda22aa850936bbf7a4d
Renne Castro <rcastro2@users.noreply.github.com>
2025-09-15 02:50:18 -0400

    adds Lesson 5 Practical

---
+++ PTLesson5Practical/main.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.bgpic("M_Maze1.gif")
+
+bob = turtle.Turtle()
+bob.penup()
+bob.goto(30,-200)
+bob.left(90)
+bob.pendown()
+
+#Challenge: Guide bob to the exit at the top
+
+
