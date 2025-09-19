ed03a78e448a8c311d96f1471b03dd1d0aa4afff
Renne Castro <rcastro2@users.noreply.github.com>
2025-09-19 04:50:03 -0400

    adds Lesson 11 Examples

---
+++ PTLesson11Examples.py
@@ -0,0 +1,31 @@
+import turtle
+
+bob = turtle.Turtle()
+bob.speed(0)
+#Example 1
+'''
+for times in range(100):
+     bob.circle(times * 5)
+     bob.forward(times * 5)
+     bob.left(120)
+'''
+#Example 2
+'''
+for times in range(100):
+     bob.circle(100)
+     bob.forward(times * 5)
+     bob.left(120)
+'''
+#Example 3
+'''
+for times in range(200):
+     bob.circle(times*2)
+     bob.forward(times*2)
+     bob.left(91)
+'''
+#Example 4
+'''
+for times in range(500):
+     bob.forward(10)
+     bob.right(times * 7)
+'''
