b5278138395b7698ff65467eadb04e0a76e51371
Renne Castro <rcastro2@users.noreply.github.com>
2025-09-19 05:00:14 -0400

    adds Lesson 12 Demonstration

---
+++ PTLesson12Demonstration/main.py
@@ -0,0 +1,11 @@
+from myfunctions import *
+bob.speed(0)
+
+for times in range(20):
+  square()
+  bob.right(92)
+
+
+
+
+
---
+++ PTLesson12Demonstration/myfunctions.py
@@ -0,0 +1,7 @@
+import turtle
+bob = turtle.Turtle()
+
+def square():
+  for times in range(4):
+    bob.forward(100)
+    bob.left(90)
